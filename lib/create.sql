9d629e0844b12decc6cbf77c3225c3b6c0c7ddc5
CiaraMaria Proia <proiacm@gmail.com>
2020-04-21 23:52:15 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,5 +1,5 @@
-CREATE TABLE projects (id INT PRIMARY KEY, title TEXT, category TEXT, funding_goal INT, start_date INT, end_date INT);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding_goal INT, start_date INT, end_date INT);
 
-CREATE TABLE users (id INT PRIMARY KEY, name TEXT, age INT);
+CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT, age INT);
 
-CREATE TABLE pledges (id INT PRIMARY KEY, amount INT, user_id INT, project_id INT);
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INT, user_id INT, project_id INT);

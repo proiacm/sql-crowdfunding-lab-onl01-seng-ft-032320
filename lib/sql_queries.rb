a4f8edb12fe61fc1c597eb33cfd0514514d4cfbc
CiaraMaria Proia <proiacm@gmail.com>
2020-04-22 00:06:16 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "SELECT title, amount FROM projects INNER JOIN pledges ON projects.title = pledges.project_id ORDER BY title;"
+  "SELECT title, amount FROM projects INNER JOIN pledges ON projects.title = pledges.project_id GROUP BY title, amount ORDER BY title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
